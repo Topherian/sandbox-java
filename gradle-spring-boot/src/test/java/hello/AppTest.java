648f1dd212537abcb3bb5b55cbe3f0d9212dbead
Topherian <topherianlimited@foo.com>
2019-01-29 23:17:15 +0000

    spring boot sandbox

---
+++ gradle-spring-boot/src/test/java/hello/AppTest.java
@@ -0,0 +1,38 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+@AutoConfigureMockMvc
+public class AppTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloGradle() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Gradle!"));
+    }
+
+    @Test
+    public void helloGreeting() throws Exception {
+        mvc.perform(get("/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+
+    }
+
+}
